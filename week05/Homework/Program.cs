8e3705ba5a9c33bb8da051c2842d7974be743386
Landau2560 <landaugrandberry256@icloud.com>
2025-09-29 15:46:40 -0700

    Finished 05 assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
+using Homework;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a = new Assignment(" Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Landau Grandberry", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Josh Waters", "European History", "The Conflict of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
